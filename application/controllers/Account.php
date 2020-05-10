95e26314dea84fba7232c5fdbbac4df3fe4693b7
Joël Elunda <joel.elunda.01@gmail.com>
2020-05-10 23:32:41 +0200

    sign in dim business

---
+++ application/controllers/Account.php
@@ -0,0 +1,130 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Account extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index() {
+		$this->load->view('head');
+		$this->load->view('sign_up');
+		$this->load->view('foot');
+	}
+
+	private function sign_up_data() { 
+		return array(
+			'name' =>  $this -> input -> post('name'),
+			'email' =>  $this -> input -> post('email'),
+			'password' =>  $this -> input -> post('password'),
+			'type' => 0
+		);
+	}
+ 
+
+	public function sign_up() {
+
+		$this -> form_validation -> set_rules('name', 'nom de l\'utilisateur', 'required', 
+			array('required' => 'Le champ %s est obligatoire.')	
+		); 
+		
+		$this -> form_validation -> set_rules('email', 'email',  'required|valid_email', 
+			array(
+				'required' => 'Le champ %s est obligatoire.',
+				'valid_email' => 'Saisissez un email valide.'
+			)
+		); 
+		
+		$this -> form_validation -> set_rules('password', 'mot de passe', 'required|min_length[8]', 
+			array(
+				'required' => 'Le champ %s est obligatoire.',
+				'min_length' => 'Le champ %s doit comporter au mois 8 caractères.'
+			)
+		); 
+
+		$this -> form_validation -> set_rules('confirm', 'confirmation', 'required|matches[password]', 
+			array(
+				'required' => 'Le champ %s est obligatoire.',
+				'matches' => 'Votre valeur de %s est différente du mot de passe.')
+		); 
+
+		if($this -> form_validation -> run()) {  
+
+			$this -> UserModel -> add($this -> sign_up_data());
+			
+			$this -> load -> view('head');
+			$this -> load -> view('index');
+			$this -> load -> view('foot');
+			 			
+		} else { 
+            $this -> load -> view('head');
+            $this -> load -> view('sign_up');
+            $this -> load -> view('foot');
+		}
+	}
+
+
+	public function login() {
+
+        $email = $this -> input -> post('email');
+		$password = $this -> input -> post('password');
+		
+        $user = array(
+            'email' => $email,
+            'password' => $password
+        );
+
+        $this -> load -> model('UserModel');
+        $result = $this -> UserModel -> check_authentification($user);
+
+        if(count($result) > 0)  {
+            $user = $result[0];
+            $user = array(
+                'id' => $user -> id,
+                'name' => $user -> name,
+                'is_connected' => true
+			);
+			
+            $this -> session -> set_userdata($user);
+            redirect('utilisateur/accueil');
+        }
+        else  {
+            $user = array(
+                'error_email' => 'email ou mot de passe incorrect.'
+            );
+            /*
+            $this -> session -> set_flashdata($user);
+            $form_auth = $this -> load -> view('utilisateur/form_authentification', [], true);
+            $user = array('page' => $form_auth);
+			
+			$this -> load -> view('mokapi_home', $user);
+			*/
+        }
+    }
+
+    public function home()  {
+        if($this -> session -> is_connected)  {
+            $this -> load -> view('utilisateur/accueil');
+        }
+        else {
+            redirect();
+        }
+    }
+
+    public function logout()   {
+        $this -> session -> unset_userdata('is_connected');
+        redirect();
+    }
+}
---
+++ application/views/sign_up.php
@@ -0,0 +1,83 @@
+
+		<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+	    <div class="container">
+	    	<a class="navbar-brand p-3" href="<?=site_url();?>">DIM BUSINESS</a>
+	    	
+	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+	        <span class="fa fa-bars"></span> Menu
+	      </button>
+	      
+	    </div>
+	  </nav>
+    <!-- END nav -->
+
+	
+	 
+
+    <section class="ftco-section bg-light">
+    	<div class="container">
+    		<div class="row justify-content-center">
+					<div class="col-md-8">
+						<div class="wrapper">
+							<div class="row no-gutters justify-content-center">
+								<div class="col-lg-8 col-md-7 order-md-last d-flex align-items-stretch">
+									<div class="contact-wrap w-100 p-md-5 p-4">
+										<h3 class="mb-4">Inscrivez-vous à l'équipe DIM BUSINESS</h3>
+										<div id="form-message-warning" class="mb-4"></div> 
+					      		 
+										<form method="POST" action="<?=site_url('account/sign_up');?>" id="contactForm" name="contactForm" class="contactForm">
+											<div class="row">
+												<div class="col-md-12">
+													<div class="form-group">
+														<label class="label" for="name">Nom d'utilisateur *</label>
+														<input type="text" class="form-control" value="<?=set_value('name');?>" name="name" id="name" placeholder="Nom d'utilisateur">
+														<small class="form-text text-muted"><?= form_error('name','<em>','</em>') ?></small>
+													</div>
+												</div>
+												<div class="col-md-12"> 
+													<div class="form-group">
+														<label class="label" for="email">Adresse Email *</label>
+														<input type="email" class="form-control" value="<?=set_value('email');?>" name="email" id="email" placeholder="Email">
+														<small class="form-text text-muted"><?= form_error('email','<em>','</em>') ?></small>
+													</div>
+												</div>
+												<div class="col-md-12">
+													<div class="form-group">
+														<label class="label" for="password">Mot de passe *</label>
+														<input type="text" class="form-control" name="password" id="password" placeholder="Mot de passe">
+														<small class="form-text text-muted"><?= form_error('password','<em>','</em>') ?></small>
+														<small class="form-text text-muted">Assurez-vous qu'il s'agit d'au moins 15 caractères OU d'au moins 8 caractères.</small>
+													</div>
+												</div>
+												<div class="col-md-12">
+													<div class="form-group">
+														<label class="label" for="confirm">Confirmer votre mot de passe *</label>
+														<input type="text" class="form-control" name="confirm" id="confirm" placeholder="Confirmation">
+														<small class="form-text text-muted"><?= form_error('confirm','<em>','</em>') ?></small>
+													</div>
+												</div>
+											 
+												<div class="col-md-12">
+													<div class="form-group">
+														<input type="submit" value="S'incrire" class="btn btn-primary w-100"> 
+													</div>
+												</div>
+												
+												<div class="col-md-12 text-left"> 
+														<small>En cliquant sur « S'inscrire », 
+														vous acceptez nos <a href="#"> <strong>conditions d'utilisation</strong> </a> et <a href="#"><strong>Déclaration de confidentialité</strong></a>. 
+														Nous vous enverrons occasionnellement des e-mails liés au compte.</small> 
+												</div>
+											
+											</div>
+										</form>
+									</div>
+								</div>
+								 
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+    	</div>
+    </section>
