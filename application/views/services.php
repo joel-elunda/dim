cbf891a011b4706ee14368d887990d5bc407f9fe
Joël Elunda <joel.elunda.01@gmail.com>
2020-05-12 11:38:29 +0200

    first services views

--- application/views/services.php
+++ application/views/services.php
@@ -2,10 +2,7 @@
 		<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
 	    <div class="container">
 	    	<a class="navbar-brand" href="index.html">DIM BUSINESS</a>
-	    	
-			<div class="searchform order-sm-start order-lg-last"> 
-				<a class="btn btn-primary" href="<?=site_url('account/login_view');?>" role="button">Se connecter</a>
-			</div>
+	    	 
 	    	
 	      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
 	        <span class="fa fa-bars"></span> Menu
@@ -24,7 +21,7 @@
     <!-- END nav -->
 
 	
-    <section class="hero-wrap hero-wrap-2" style="background-image: url('images/bg_2.jpg');" data-stellar-background-ratio="0.5">
+    <section class="hero-wrap hero-wrap-2" style="background-image: url(<?=base_url('assets/images/bg_2.jpg');?>);" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text align-items-end">
@@ -38,6 +35,15 @@
 
     <section class="ftco-section">
     	<div class="container">
+
+
+			<div class="row justify-content-center pb-5 mb-3">
+				<div class="col-md-7 heading-section text-center ftco-animate">
+					<span class="subheading">Services</span>
+					<h2>Notre Business Solutions</h2>
+				</div>
+			</div>
+
     		<div class="row">
           <div class="col-md-6 col-lg-3 d-flex services align-self-stretch px-4 ftco-animate">
             <div class="d-block">
@@ -45,8 +51,8 @@
             		<span class="flaticon-accounting-1"></span>
               </div>
               <div class="media-body">
-                <h3 class="heading">Accounting</h3>
-                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+                <h3 class="heading">MEDIA</h3>
+                <p>Promotion du potentiel de créativité de l'écosystem entrepreneurial congolais jusqu'à l'étranger.</p>
               </div>
             </div>      
           </div>
@@ -56,8 +62,8 @@
             		<span class="flaticon-tax"></span>
               </div>
               <div class="media-body">
-                <h3 class="heading">Tax, Compliance &amp; Payroll</h3>
-                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+                <h3 class="heading">CABINET D'ETUDE DES PROJETS</h3>
+                <p>Agro-DIM projet d'incubation du secteur agricol accompagnant les acteurs du domaine dans la création des chainesd de valeur.</p>
               </div>
             </div>    
           </div>
@@ -67,8 +73,8 @@
             		<span class="flaticon-loan"></span>
               </div>
               <div class="media-body">
-                <h3 class="heading">Financial Services</h3>
-                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+                <h3 class="heading">NUMERISATION ET TRANSFORMATION</h3>
+                <p>Etude et réalisation des numériques, technologiques et autres domaines grâce aux start-ups et partenaires affilié à notre plateforme.</p>
               </div>
             </div>      
           </div>
@@ -78,160 +84,12 @@
             		<span class="flaticon-budget"></span>
               </div>
               <div class="media-body">
-                <h3 class="heading">Growth &amp; Funding Access</h3>
-                <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic.</p>
+                <h3 class="heading">CROWD FOUNDING</h3>
+                <p>Financement participatif des projets innovants et des actions bénévoles grâce à notre holding numérisé.</p>
               </div>
             </div>      
           </div>
         </div>
     	</div>
     </section>
-
-    <section class="ftco-section ftco-no-pt bg-light">
-    	<div class="container">
-    		<div class="row d-flex no-gutters">
-    			<div class="col-md-6 d-flex">
-    				<div class="img img-video d-flex align-self-stretch align-items-center justify-content-center justify-content-md-center mb-4 mb-sm-0" style="background-image:url(images/about.jpg);">
-    				</div>
-    			</div>
-    			<div class="col-md-6 pl-md-5 py-md-5">
-    				<div class="heading-section pl-md-4 pt-md-5">
-    					<span class="subheading">Welcome to Accounting</span>
-	            <h2 class="mb-4">We Are the Best Accounting Agency</h2>
-    				</div>
-    				<div class="services-2 w-100 d-flex">
-    					<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-wealth"></span></div>
-    					<div class="text pl-4">
-    						<h4>Market Analysis</h4>
-    						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-    					</div>
-    				</div>
-    				<div class="services-2 w-100 d-flex">
-    					<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-accountant"></span></div>
-    					<div class="text pl-4">
-    						<h4>Accounting Advisor</h4>
-    						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-    					</div>
-    				</div>
-    				<div class="services-2 w-100 d-flex">
-    					<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-teamwork"></span></div>
-    					<div class="text pl-4">
-    						<h4>General Consultancy</h4>
-    						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-    					</div>
-    				</div>
-    				<div class="services-2 w-100 d-flex">
-    					<div class="icon d-flex align-items-center justify-content-center"><span class="flaticon-accounting"></span></div>
-    					<div class="text pl-4">
-    						<h4>Structured Assestment</h4>
-    						<p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-    					</div>
-    				</div>
-	        </div>
-        </div>
-    	</div>
-    </section>
-
-    <section class="ftco-section bg-light ftco-no-pt">
-    	<div class="container">
-    		<div class="row justify-content-center pb-5 mb-3">
-          <div class="col-md-7 heading-section text-center ftco-animate">
-          	<span class="subheading">Price &amp; Plans</span>
-            <h2>Affordable Packages</h2>
-          </div>
-        </div>
-    		<div class="row">
-    			<div class="col-md-6 col-lg-3 ftco-animate">
-	          <div class="block-7">
-	            <div class="text-center">
-	            	<span class="excerpt d-block">Personal</span>
-	            <span class="price"><sup>$</sup> <span class="number">49</span> <sub>/mos</sub></span>
-	            
-	            <ul class="pricing-text mb-5">
-	              <li><span class="fa fa-check mr-2"></span>Brand Strategy</li>
-	              <li><span class="fa fa-check mr-2"></span>Online Marketing</li>
-	              <li><span class="fa fa-check mr-2"></span>Branding Services</li>
-	              <li><span class="fa fa-check mr-2"></span>Creative Marketing</li>
-	              <li><span class="fa fa-check mr-2"></span>Sales Management</li>
-	            </ul>
-
-	            <a href="#" class="btn btn-primary d-block px-2 py-3">Get Started</a>
-	            </div>
-	          </div>
-	        </div>
-	        <div class="col-md-6 col-lg-3 ftco-animate">
-	          <div class="block-7">
-	            <div class="text-center">
-            	<span class="excerpt d-block">Business</span>
-	            <span class="price"><sup>$</sup> <span class="number">79</span> <sub>/mos</sub></span>
-	            
-	            <ul class="pricing-text mb-5">
-	              <li><span class="fa fa-check mr-2"></span>Brand Strategy</li>
-	              <li><span class="fa fa-check mr-2"></span>Online Marketing</li>
-	              <li><span class="fa fa-check mr-2"></span>Branding Services</li>
-	              <li><span class="fa fa-check mr-2"></span>Creative Marketing</li>
-	              <li><span class="fa fa-check mr-2"></span>Sales Management</li>
-	            </ul>
-
-	            <a href="#" class="btn btn-primary d-block px-2 py-3">Get Started</a>
-	            </div>
-	          </div>
-	        </div>
-	        <div class="col-md-6 col-lg-3 ftco-animate">
-	          <div class="block-7">
-	            <div class="text-center">
-	            	<span class="excerpt d-block">Ultimate</span>
-	            <span class="price"><sup>$</sup> <span class="number">109</span> <sub>/mos</sub></span>
-	            
-	            <ul class="pricing-text mb-5">
-	              <li><span class="fa fa-check mr-2"></span>Brand Strategy</li>
-	              <li><span class="fa fa-check mr-2"></span>Online Marketing</li>
-	              <li><span class="fa fa-check mr-2"></span>Branding Services</li>
-	              <li><span class="fa fa-check mr-2"></span>Creative Marketing</li>
-	              <li><span class="fa fa-check mr-2"></span>Sales Management</li>
-	            </ul>
-
-	            <a href="#" class="btn btn-primary d-block px-2 py-3">Get Started</a>
-	            </div>
-	          </div>
-	        </div>
-	        <div class="col-md-6 col-lg-3 ftco-animate">
-	          <div class="block-7">
-	            <div class="text-center">
-	            	<span class="excerpt d-block">Premium</span>
-	            <span class="price"><sup>$</sup> <span class="number">149</span> <sub>/mos</sub></span>
-	            
-	            <ul class="pricing-text mb-5">
-	              <li><span class="fa fa-check mr-2"></span>Brand Strategy</li>
-	              <li><span class="fa fa-check mr-2"></span>Online Marketing</li>
-	              <li><span class="fa fa-check mr-2"></span>Branding Services</li>
-	              <li><span class="fa fa-check mr-2"></span>Creative Marketing</li>
-	              <li><span class="fa fa-check mr-2"></span>Sales Management</li>
-	            </ul>
-
-	            <a href="#" class="btn btn-primary d-block px-2 py-3">Get Started</a>
-	            </div>
-	          </div>
-	        </div>
-	      </div>
-    	</div>
-    </section>
-
-    <section class="ftco-section ftco-no-pb ftco-no-pt bg-secondary">
-      <div class="container py-5">
-    		<div class="row">
-          <div class="col-md-7 d-flex align-items-center">
-            <h2 class="mb-3 mb-sm-0" style="color:black; font-size: 22px;">Sign Up for Your Free 1st Accounting Consultation</h2>
-          </div>
-          <div class="col-md-5 d-flex align-items-center">
-            <form action="#" class="subscribe-form">
-              <div class="form-group d-flex">
-                <input type="text" class="form-control" placeholder="Enter email address">
-                <input type="submit" value="Subscribe" class="submit px-3">
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </section>
-
+ 
