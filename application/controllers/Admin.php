a79d881c92a58d1d93502819bcc63283e9a1aa17
Joël Elunda <joel.elunda.01@gmail.com>
2020-05-18 22:18:02 +0200

    login view admin

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -7,15 +7,62 @@ class Admin extends CI_Controller {
 		$this->load->view('admin/index'); 
 	}
 
+	public function admin_data() {
+		return array(
+			'email' => $this -> input -> post('email'),
+			'password' => $this -> input -> post('password')
+		);
+	}
+
+
+	public function login() {
+		$this -> form_validation -> set_rules('email', 'email',  'required|valid_email', 
+			array(
+				'required' => 'Le champ %s est obligatoire.',
+				'valid_email' => 'Saisissez un email valide.'
+			)
+		); 
+		
+		$this -> form_validation -> set_rules('password', 'mot de passe', 'required|min_length[8]', 
+			array(
+				'required' => 'Le champ %s est obligatoire.',
+				'min_length' => 'Le champ %s doit comporter au mois 8 caractères.'
+			)
+		); 
+
+		if($this -> form_validation -> run()) {
+			print_r($this -> admin_data());
+
+			/*
+			$this -> dataModel -> add($this -> sign_up_data());
+
+			$data = $this -> sign_up_data();
+			$result = $this -> dataModel -> check_authentification($data);
+
+			if(count($result) > 0)  {
+				$data = $result[0];
+				$data = array(
+					'id' => $data -> id,
+					'name' => $data -> name,
+					'email' => $data -> email,
+					'is_connected' => true
+				);
+				
+				$this -> session -> set_userdata($data);
+				redirect('account/home');
+			*/
+		} else {
+			print_r($this -> admin_data());
+			$this -> login_view();
+		}
+	}
 
 
 
 
-
-
-
-
-
+	public function login_view() {
+		$this->load->view('admin/login'); 
+	}
 
 
 	public function profile() {
