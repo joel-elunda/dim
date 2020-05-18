058c79d329f36ec4cab5da3cc3f587a0d38eea4f
Joël Elunda <joel.elunda.01@gmail.com>
2020-05-18 20:51:35 +0200

    .php

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -6,4 +6,36 @@ class Admin extends CI_Controller {
 	public function index() {  
 		$this->load->view('admin/index'); 
 	}
+
+
+
+
+
+
+
+
+
+
+
+	
+
+	public function _404() {
+		$this->load->view('admin/404'); 
+	}
+
+	public function basic_table() {
+		$this->load->view('admin/basic-table'); 
+	}
+
+	public function blank() {
+		$this->load->view('admin/blank'); 
+	}
+
+	public function map_google() {
+		$this->load->view('admin/map-google'); 
+	}
+
+	public function profile() {
+		$this->load->view('admin/profile'); 
+	}
 }
